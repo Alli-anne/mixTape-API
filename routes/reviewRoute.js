b40fec5d26c095ae8d46b84b6556a311d6cd8669
Alli-anne <145496176+Alli-anne@users.noreply.github.com>
2025-12-04 20:40:32 -0700

    review changes

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,9 +1,22 @@
 import express from 'express';
-import { getAllReviews } from '../controller/reviewController.js';
+import { getAllReviews,
+  getReviewById,
+  createReview,
+  updateReview,
+  deleteReview } from '../controller/reviewController.js';
 import { requiresAuth } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get('/', requiresAuth, getAllReviews);
+router.get('/', requiresAuth(), getAllReviews);
+
+router.get('/:id', requiresAuth(), getReviewById);
+
+router.post('/', requiresAuth(), createReview);
+
+router.put('/:id', requiresAuth(), updateReview);
+
+router.delete('/:id', requiresAuth(), deleteReview);
+
 
 export default router;
