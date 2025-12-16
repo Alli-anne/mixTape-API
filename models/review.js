5047a99a966d524af9e073f8afbea50541608c97
Alli-anne <145496176+Alli-anne@users.noreply.github.com>
2025-12-15 18:09:21 -0700

    review schema working?

--- models/review.js
+++ models/review.js
@@ -1,8 +1,27 @@
 import mongoose from 'mongoose';
 
 const reviewSchema = new mongoose.Schema({
-    
-})
+  songId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Song",
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+  comment: {
+    type: String
+  }
+}, { timestamps: true });
+
 
 reviewSchema.pre('save', function (next) {
     this.updatedAt = new Date();
